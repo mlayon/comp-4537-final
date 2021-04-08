e54ee54fef14fafb214427c109c4f1f1cb016e30
Mikhaela Layon <mapl.layon@gmail.com>
2021-04-07 19:41:40 -0700

    now able to create and delete comments

--- client/js/post.js
+++ client/js/post.js
@@ -28,21 +28,42 @@ function renderPost(post) {
 // renders dom elements for each post
 function renderComment(comment) {
 	console.log("Comments", comment);
+    let commentId = comment.comment_id;
 
 	// post related elements
 	let row = document.createElement("tr");
 	let td_content = document.createElement("td");
+    let td_edit = document.createElement("td");
+    let td_delete = document.createElement("td");
 
-	row.id = comment.comment_id;
+    let button_edit = document.createElement("button");
+    let button_delete = document.createElement("button");
+    button_edit.classList.add("editComment");
+    button_delete.classList.add("deleteComment");
+    button_edit.id = commentId;
+    button_delete.id = commentId;
+
+    let node_edit = document.createTextNode("Edit Comment");
+    let node_delete = document.createTextNode("Delete Comment");
+
+    button_edit.appendChild(node_edit);
+    button_delete.appendChild(node_delete);
+
+    td_edit.appendChild(button_edit);
+    td_delete.appendChild(button_delete);
+
+	row.id = commentId;
 	td_content.innerHTML = comment.content;
 
 	// appending elements
 	row.appendChild(td_content);
+    row.appendChild(td_edit);
+    row.appendChild(td_delete);
 
 	document.getElementById("comments-table").appendChild(row);
 }
 
-function configureButtons() {
+function configurePostButtons() {
 	$("#editButton").click(function () {
 		window.location = "./edit.html?id=" + postId;
 	});
@@ -53,7 +74,8 @@ function configureButtons() {
 		alert("Post deleted");
 		return (window.location = "./index.html");
 	});
-
+    
+    // Save button for edit.html
 	$("#saveButton").click(function () {
 		let title = document.getElementById("title").value;
 		let topic = document.getElementById("topic").value;
@@ -64,7 +86,38 @@ function configureButtons() {
 		alert("Post updated");
 		return (window.location = "./post.html?id=" + postId);
 	});
+
+    $("#addComment").click(function () {
+		let comment = document.getElementById("comment").value;
+        if (comment) {
+            createComment(comment, postId);
+		    alert("Comment added");
+        }
+        return (window.location = "./post.html?id=" + postId);
+	});
+    
 }
 
-configureButtons();
+function configureCommentButtons() {
+    $("#addComment").click(function () {
+		let comment = document.getElementById("comment").value;
+        if (comment) {
+            createComment(comment, postId);
+		    alert("Comment added");
+        }
+        return (window.location = "./post.html?id=" + postId);
+	});
+
+    $(document).on('click', '.deleteComment', function() { 
+        let commentId = this.id;
+        deleteComment(commentId);
+		alert("Comment deleted");
+		return (window.location = "./post.html?id=" + postId);
+    });
+
+
+}
+
+configurePostButtons();
+configureCommentButtons();
 loadComments();
